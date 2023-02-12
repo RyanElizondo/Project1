481c9b596914b7b4b108011f0955940fa4c81ac9
Alex Virilli <124116623+alexvirilli@users.noreply.github.com>
2023-02-12 13:30:58 -0500

    Update RosterManager.java

--- RosterManager.java
+++ RosterManager.java
@@ -1,41 +1,34 @@
 import java.util.Scanner;
 public class RosterManager {
-    Scanner sc = new Scanner(System.in);
-    Roster roster = new Roster();
-    major[] majors = major.values();
+    Scanner sc = new Scanner(System.in); Roster roster = new Roster(); Major[] majors = Major.values();
     void run() {
+        Scanner sc = new Scanner(System.in); Roster roster = new Roster(); Major[] majors = Major.values(); Checker check = new Checker();
         System.out.println("Roster Manager is running...");
         while (true) {
-            String command = sc.nextLine();
-            String[] line = command.split("\\s+");
-            if (line[0].equals("A")) { //add a new student
-                Student student = new Student(new Profile(line[1], line[2], new date(line[3])), majors[Integer.parseInt(line[4])], Integer.parseInt(line[5]));
-                roster.add(student);
+            String command = sc.nextLine(); String[] line = command.split("\\s+");
+            if(line.length==0) continue;
+            else if(line[0].trim().length()==0) continue;
+            if(line[0].equals("A")) { //add a new student
+                Date date = new Date(line[3]);
+                if(check.isValidToAdd(date, line)){
+                    Profile profile = new Profile(line[2],line[1],date);
+                    Student student = check.makeStudent(profile,majors[Major.CS.getMajorIndex(line[4])],Integer.parseInt(line[5]));
+                    if (!roster.contains(student)) { roster.add(student); System.out.println(profile.toString() + " added to the roster.");
+                    } else { System.out.println(profile.toString() + " is already in the roster."); }}
             } else if (line[0].equals("R")) { //remove a student
-                Student student = new Student(new Profile(line[1], line[2], new date(line[3])), majors[Integer.parseInt(line[4])], Integer.parseInt(line[5]));
-                if (roster.contains(student)){
-                    roster.remove(student);
-                }
-            } else if (line[0].equals("P")) { //display roster sorted by last name, first name, and DOB
-                roster.print();
-            } else if (line[0].equals("PS")) { //display roster sorted by standing
-                roster.printByStanding();
-            } else if (line[0].equals("PC")){ //display roster sorted by school and major
-                roster.printBySchoolMajor();
-            } else if (line[0].equals("L")) { //list student in a specified school
-
+                Date date = new Date(line[3]);
+                Profile profile = new Profile(line[2],line[1],date);
+                Student student = check.makeStudent(profile, Major.CS, 0);
+                if (roster.remove(student)) { System.out.println(profile.toString() + " removed from the roster.");
+                } else { System.out.println(profile.toString() + " is not in roster.");}
+            } else if (line[0].equals("P")) { /*display roster sorted by last name, first name, and DOB*/ roster.print();
+            } else if (line[0].equals("PS")) { /*display roster sorted by standing*/ roster.printByStanding();
+            } else if (line[0].equals("PC")){ /*display roster sorted by school and major*/ roster.printBySchoolMajor();
+            } else if (line[0].equals("L")) { /*list student in a specified school*/ roster.printSchool(line[1]);
             } else if(line[0].equals("C")){ //change a students major
-                Student student = new Student(new Profile(line[1], line[2], new date(line[3])), majors[Integer.parseInt(line[4])], Integer.parseInt(line[5]));
-                if(roster.contains(student)){
-                    roster.changeMajor(student, majors[Integer.parseInt(line[5])]);
-                }
-            } else if(line[0].equals("Q")){ //terminate run
-                System.out.println("Roster Manager terminated.");
-                break;
-            } else {
-                System.out.println(line[0] + "is an invalid command");
-            }
+                check.changeStudentMajor(line,roster,majors);
+            } else if(line[0].equals("Q")){ /*terminate run*/ System.out.println("Roster Manager terminated."); break;
+            } else { System.out.println(line[0] + " is an invalid command!");}
         }
     }
 }
-
