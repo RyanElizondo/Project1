70633ddee72af76690a4ffc553217340535d0101
Ryan Elizondo-Fallas <ryanelizondo10@gmail.com>
2023-02-13 19:39:19 -0500

    Update RunProject1.java

--- RunProject1.java
+++ RunProject1.java
@@ -1,3 +1,10 @@
+package roster;
+
+/**
+ * This class is the main driver class for project 1.
+ * It creates a new RosterManager object and calls its run method.
+ * @Author: Alex Virilli, Ryan Elizondo-Fallas
+ */
 public class RunProject1 {
     public static void main(String[] args){
         new RosterManager().run();
